7bf5d07398bbf3cdb38d85d88d7754dca079ad20
cazaciuc gabriel <gcazaciuc@yahoo.com>
2017-05-15 13:24:07 +0300

    Update webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         exclude: [/node_modules/],
         use: [{
           loader: 'babel-loader',
-          options: { presets: ['es2015', 'react'] },
+          options: { presets: ['es2015'] },
         }],
       }
     ]
